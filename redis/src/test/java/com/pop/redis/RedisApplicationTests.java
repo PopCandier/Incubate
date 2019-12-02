ca759fb11adfa4656c565c5e80e985091cbaa84e
Pop <824173124@qq.com>
2019-12-02 17:55:07 +0800

    孵化工程-redis

---
+++ redis/src/test/java/com/pop/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pop.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
